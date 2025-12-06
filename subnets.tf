744960cfe259048d2816cda808cc363db571df24
ItaroBase <itamar.rosenblum@gmail.com>
2025-12-06 14:15:52 +0100

    Fix subnet VPC reference

--- subnets.tf
+++ subnets.tf
@@ -1,6 +1,6 @@
 # Create Private Subnet 1 in the VPC
 resource "aws_subnet" "public_subnet_1" {
-  vpc_id                  = aws_vpc.aws_capstone_vpc.id
+  vpc_id                  = aws_vpc.wordpress_vpc.id
   cidr_block              = "10.0.2.0/28"
   availability_zone       = "us-west-2a"
   map_public_ip_on_launch = true
