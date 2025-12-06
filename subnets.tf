58858d22b0089f5940cf70652135db6257af8ce8
ItaroBase <itamar.rosenblum@gmail.com>
2025-12-06 14:08:05 +0100

    Adding public subnet

---
+++ subnets.tf
@@ -0,0 +1,10 @@
+# Create Private Subnet 1 in the VPC
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id                  = aws_vpc.aws_capstone_vpc.id
+  cidr_block              = "10.0.2.0/28"
+  availability_zone       = "us-west-2a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "PublicSubnet1"
+  }
+}
