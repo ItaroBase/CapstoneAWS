249b97c7ba7859d7c571df7b8efbe45a6f92d1ca
ItaroBase <itamar.rosenblum@gmail.com>
2025-12-06 15:05:51 +0100

    Creating Security group

--- public_route_table.tf
+++ public_route_table.tf
@@ -1,5 +1,5 @@
 # 1. Public Route Table
-resource "aws_route_table" "public_rt" {
+resource "aws_route_table" "public_route_table" {
   vpc_id = aws_vpc.wordpress_vpc.id
 
   tags = {
---
+++ security_groups.tf
@@ -0,0 +1,34 @@
+resource "aws_security_group" "wordpress_sg" {
+  name        = "wordpress_sg"
+  description = "Allow HTTP and SSH"
+  vpc_id      = aws_vpc.wordpress_vpc.id
+
+  # Inbound rules
+  ingress {
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow SSH from your IP"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["YOUR.IP.ADDRESS/32"]
+  }
+
+  # Outbound rules (all allowed)
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "wordpress_sg"
+  }
+}
