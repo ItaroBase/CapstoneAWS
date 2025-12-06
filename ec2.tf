64fdf7df27073e907a4e437157b62510912da117
ItaroBase <itamar.rosenblum@gmail.com>
2025-12-06 16:03:11 +0100

    Updating correct AMI

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "wordpress_server" {
-  ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2023 in us-west-2
+  ami           = "ami-02b297871a94f4b42" # Amazon Linux 2023 in us-west-2
   instance_type = "t2.micro"
 
   subnet_id                   = aws_subnet.public_subnet_1.id
