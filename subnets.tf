6d7e6c3e6827ad0f87fa6ebd871d58d759734759
ItaroBase <itamar.rosenblum@gmail.com>
2025-12-06 14:26:25 +0100

    Fix CIDR range of public subnet

--- subnets.tf
+++ subnets.tf
@@ -1,7 +1,7 @@
 # Create Private Subnet 1 in the VPC
 resource "aws_subnet" "public_subnet_1" {
   vpc_id                  = aws_vpc.wordpress_vpc.id
-  cidr_block              = "10.0.2.0/28"
+  cidr_block              = "10.0.0.0/28"
   availability_zone       = "us-west-2a"
   map_public_ip_on_launch = true
   tags = {
