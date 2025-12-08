9dc38e49fec00a735427a0def4a01208592221b1
ItaroBase <itamar.rosenblum@gmail.com>
2025-12-08 18:28:14 +0100

    Add bastion host + update security groups

--- ec2.tf
+++ ec2.tf
@@ -1,3 +1,18 @@
+resource "aws_instance" "bastion_host" {
+  ami           = "ami-02b297871a94f4b42"
+  instance_type = "t2.micro"
+
+  subnet_id                   = aws_subnet.public_subnet_1.id
+  vpc_security_group_ids      = [aws_security_group.aws_ssh_sg.id]
+  associate_public_ip_address = true
+
+  user_data = file("bastion-host-userdata.sh")
+
+  tags = {
+    Name = "Bastion-Host"
+  }
+}
+
 resource "aws_instance" "wordpress_server" {
   ami           = "ami-02b297871a94f4b42" # Amazon Linux 2023 in us-west-2
   instance_type = "t2.micro"
--- security_groups.tf
+++ security_groups.tf
@@ -1,3 +1,31 @@
+# Bastion Host security security group'
+
+resource "aws_security_group" "aws_ssh_sg" {
+  name        = "aws_ssh_sg"
+  description = "Allow SSH from your IP to Bastion Host"
+  vpc_id      = aws_vpc.wordpress_vpc.id
+
+  ingress {
+    description = "SSH from your IP"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["95.90.247.241/32"]  # <-- My IP
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "aws_ssh_sg"
+  }
+}
+
+# WordPress security group
 resource "aws_security_group" "wordpress_sg" {
   name        = "wordpress_sg"
   description = "Allow HTTP and SSH"
@@ -12,11 +40,11 @@ resource "aws_security_group" "wordpress_sg" {
   }
 
   ingress {
-    description = "Allow SSH from your IP"
+    description = "Allow SSH ONLY from Bastion Host"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["95.90.247.241/32"]  # <-- YOUR REAL IP HERE
+    security_groups = [aws_security_group.aws_ssh_sg.id]
   }
 
   egress {
